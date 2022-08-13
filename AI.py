f8347bbab43594f99ca4b45e993d401ec9b493ea
Advait <95575845+Advait1221@users.noreply.github.com>
2022-08-13 19:09:58 +0530

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,7 @@
+Name = input("Enter Your Name\n")
+print("Welcome",Name,"What Can I Do For You")
+AI = input("\n")
+#Food
+if AI=="Can You Order Pizza":
+ print("Yes","https://www.ubereats.com/")
+ #still developing
---
+++ Canculator.py
@@ -0,0 +1,21 @@
+a = input("please enter the number 1\n")
+a =float(a)
+b =input("please enter the second number\n")
+b =float(b)
+c = input("please input the opperation\n")
+p = "+"
+s = "-"
+m = "*"
+m1 = "x"
+d = "/"
+print("Let me calculate")
+if c==p:
+ print(a+b)
+elif c==s:
+ print(a-b)
+elif c==m:
+ print(a*b)
+elif c==m1:
+ print(a*b)
+elif c==d:
+ print(a/b)
